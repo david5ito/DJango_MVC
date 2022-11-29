a3c62f726ee15d7958dfca8ff1d6da88da97597c
David.Ch <96040641+david5ito@users.noreply.github.com>
2022-11-28 19:34:18 -0600

    Add files via upload

---
+++ sistema/libreria/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+    
+
+def libros(request):
+     libros = Libro.objects.all()
+     print (libros)
+     return render(request, 'libros/index.html', {'libros': libros})
+
+def crear(request):
+     formulario = LibroForm(request.POST or None)
+     if formulario.is_valid():
+          formulario.save()
+          return redirect('libros')
+     return render(request, 'libros/crear.html', {'formulario' : formulario})
+
+
+def editar(request, id):
+     libro = Libro.objects.get(id=id)
+     formulario = LibroForm(request.POST or None, instance=libro)
+     if formulario.is_valid() and request.POST:
+          formulario.save()
+          return redirect('libros')
+     return render(request, 'libros/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+     libros = Libro.objects.get(id=id)
+     libros.delete()
+     return redirect('libros')
+     
+
+    
+    
